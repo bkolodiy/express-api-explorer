5b6a637ca3b6f00b37fb65015d16fc1f15d04386
Roger <worfandsheep@qq.com>
2017-08-26 18:17:13 +0800

    working

---
+++ build/webpack.config.js
@@ -0,0 +1,18 @@
+import path from 'path'
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, `../dist/`),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import apiExplorer from '../src/index'
+
+var mainRouter = express.Router();
+var userRouter = express.Router();
+var blogRouter = express.Router();
+var addressRouter = express.Router();
+
+addressRouter.get('/:addressId', getAddressHandler);
+
+userRouter.post('/login', loginHandler);
+userRouter.get('/logout', logoutHandler);
+
+blogRouter.get('/:blogId', getBlogHandler);
+
+userRouter.use('/address', addressRouter);
+mainRouter.use('/user', userRouter);
+mainRouter.use('/blog', blogRouter);
+mainRouter.get('/status', statusHandler);
+
+var app = express();
+app.use('/api', mainRouter);
+
+function statusHandler(req, res, next) {
+  res.json('logoutHandler')
+}
+
+function logoutHandler(req, res, next) {
+  res.json('logoutHandler')
+}
+
+function loginHandler(req, res, next) {
+  res.json('loginHandler')
+}
+
+function getBlogHandler(req, res, next) {
+  res.json('getBlogHandler')
+}
+
+function getAddressHandler(req, res, next) {
+  res.json('getAddressHandler')
+}
+
+apiExplorer(app, '/api');
+
+app.listen(8080, '127.0.0.1');
